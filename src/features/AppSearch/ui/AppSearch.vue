dc0ff0737451d8c80de0f3d5778915e493e15e70
EndlessC0ding <endlesscoding@yahoo.com>
2025-03-19 22:02:48 +0300

    ch AFld

--- src/features/AppSearch/ui/AppSearch.vue
+++ src/features/AppSearch/ui/AppSearch.vue
@@ -1,11 +1,32 @@
 <script setup lang="ts">
 import AppField from '@/shared/ui/AppField/AppField.vue';
+import AppIcon from '@/shared/ui/AppIcon/AppIcon.vue';
+import AppTypography from '@/shared/ui/AppTypography/AppTypography.vue';
 
-
+const onChangeSearch = (value: string) => {
+  console.log("value", value);
+  return "";
+};
 </script>
 
 <template>
-  <div>
-    <AppField />
+  <div style="display: flex;">
+    <AppField
+      :onChange="onChangeSearch"
+      :placeholder="'Найти товар'"
+    >
+      <template #label>
+        <AppTypography>
+          Text
+        </AppTypography>
+      </template>
+      <template #rightIcon>
+        <AppIcon type="search" />
+      </template>
+      <template #leftIcon>
+        <AppIcon type="search" />
+      </template>
+    </AppField>
+
   </div>
 </template>
--- src/shared/ui/AppField/AppField.vue
+++ src/shared/ui/AppField/AppField.vue
@@ -1,33 +1,144 @@
 <script lang="ts" setup>
-import AppTypography from '../AppTypography/AppTypography.vue';
+import { useSlots } from 'vue';
 
 interface Props {
   disabled?: boolean;
   size?: "l" | "m";
   placeholder?: string;
-}
 
-const { disabled, size = "m", placeholder = "Type text" } = defineProps<Props>()
+  onChange: (value: string) => void;
+}
+// interface Emits {
+//   onChange: (value: string) => void;
+// }
+// const { onChange } = defineEmits<Emits>();
+
+
+const { disabled = false, size = "m", placeholder, onChange } = defineProps<Props>();
+const slots = useSlots();
 
 </script>
 <template>
-  <div class="field">
-    <slot name="label"></slot>
+  <div :class="[
+    'field',
+    `disabled_${disabled}`,
+    `size_${size}`,
+    `left-icon_${Boolean(slots.leftIcon)}`,
+    `right-icon_${Boolean(slots.rightIcon)}`
+  ]">
+    <slot
+      v-if="slots.label"
+      class="field__label"
+      name="label"
+    ></slot>
     <div class="field__container">
-      <slot name="leftIcon"></slot>
+      <div
+        v-if="slots.leftIcon"
+        class="field__left-icon"
+      >
+        <slot name="leftIcon"></slot>
+      </div>
       <input
+        @input="(e: Event) => onChange?.((e.target as HTMLInputElement).value)"
         :disabled="disabled"
-        :class="['field__input', 'size_${ size }']"
+        :class="['field__input', `size_${size}`]"
         :placeholder="placeholder"
         type="text"
       />
-      <slot name="rightIcon"></slot>
+      <div
+        v-if="slots.rightIcon"
+        class="field__right-icon"
+      >
+        <slot name="rightIcon"></slot>
+      </div>
     </div>
   </div>
 
 </template>
 <style scoped>
-.field {
-  display: block;
+.field:deep(.typography) {
+  color: var(--grayscale-hard);
+}
+
+.field__container {
+  position: relative;
+}
+
+.field__left-icon,
+.field__right-icon {
+  position: absolute;
+}
+
+
+.field__input {
+  width: 100%;
+  border-radius: 4px;
+  border: 1px solid var(--grayscale-light);
+  background: var(--main-surface);
+  color: var(--main-on-surface);
+}
+
+.field__input:focus {
+  border: 1px solid var(--main-secondary);
+  caret-color: var(--main-secondary);
+  box-shadow: var(--shadow-secondary-m);
+  transition: all ease-in-out .3s;
+}
+
+.field__input ::placeholder {
+  color: var(--grayscale-hard);
+}
+
+.field__input:disabled {
+  border: 1px solid var(--grayscale-light);
+  background: var(--grayscale-light);
+}
+
+.field.size_m .field__input {
+  padding: 8px 16px;
+  font-size: 16px;
+}
+
+.field.size_l .field__input {
+  padding: 12px 16px;
+  font-size: 24px;
+}
+
+.field.left-icon_true.size_m .field__left-icon {
+  top: 8px;
+  left: 8px;
+}
+
+.field.left-icon_true.size_m .field__input {
+  padding-left: 40px;
+}
+
+.field.right-icon_true.size_m .field__right-icon {
+  top: 8px;
+  right: 8px;
+}
+
+.field.right-icon_true.size_m .field__input {
+  padding-right: 40px;
+}
+
+.field.left-icon_true.size_l .field__left-icon {
+  top: 8px;
+  left: 8px;
+  padding: 6px;
+}
+
+.field.left-icon_true.size_l .field__input {
+  padding-left: 56px;
+}
+
+.field.right-icon_true.size_l .field__right-icon {
+  top: 8px;
+  right: 8px;
+  padding: 6px;
+}
+
+.field.right-icon_true.size_l .field__input {
+  padding-right: 56px;
 }
 </style>
